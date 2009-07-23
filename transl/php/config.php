70699a0d5ed8641b2de7f297c2baf53a99062de7
Paul Vriens <Paul.Vriens.Wine@gmail.com>
2009-07-23 09:39:24 +0200

    transl: Use a correct link for the 1.0.x translation status.

--- transl/php/config.php
+++ transl/php/config.php
@@ -1,5 +1,5 @@
 <?php
 $DATAROOT = ".";
 $TITLE_APPENDIX = " - Wine 1.1.x";
-$TITLE_DESCR = "This page shows the translation status of the most recent Wine 1.1.x. To see the Wine 1.0.x line see <a href=\"http://pf128.krakow.sdi.tpnet.pl/wine-transl/\">here</a>";
+$TITLE_DESCR = "This page shows the translation status of the most recent Wine 1.1.x. To see the Wine 1.0.x line see <a href=\"http://www.mikolaj.zalewski.pl/wine-transl/1.0.x/\">here</a>";
 ?>
