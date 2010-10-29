0aa5f7e4caebe9493fbf63db5ad37bf512a7854e
Ge van Geldorp <ggeldorp@vmware.com>
2010-10-29 12:47:41 +0200

    Handle updates to configure/configure.ac

--- bin/BuildSingleTest.pl
+++ bin/BuildSingleTest.pl
@@ -12,6 +12,17 @@ use lib "$Dir/../lib";
 
 use WineTestBot::Config;
 
+sub InfoMsg
+{
+  my $oldumask = umask(002);
+  if (open LOGFILE, ">>$LogDir/BuildSingleTest.log")
+  {
+    print LOGFILE @_;
+    close LOGFILE;
+  }
+  umask($oldumask);
+}
+
 sub LogMsg
 {
   my $oldumask = umask(002);
@@ -34,61 +45,101 @@ sub ApplyPatch
 {
   my ($PatchFile, $PatchType, $BaseName) = @_;
 
-  my $NeedConfig = 0;
+  my $NeedMakefile = 0;
   my $NeedMakeInclude = !1;
   my $NeedBuildDeps = !1;
   my $NeedImplib = !1;
+  my $NeedAutoconf = !1;
+  my $NeedConfigure = !1;
   if (open (FH, "<$PatchFile"))
   {
     my $Line;
     while (defined($Line = <FH>) &&
-           ($NeedConfig == 0 || ! $NeedMakeInclude || ! $NeedBuildDeps ||
-            ! $NeedImplib))
+           ($NeedMakefile == 0 || ! $NeedMakeInclude || ! $NeedBuildDeps ||
+            ! $NeedImplib || ! $NeedAutoconf || ! $NeedConfigure))
     {
-      if ($Line =~ m=tests/Makefile\.in=)
+      if ($Line =~ m=^diff.*tests/Makefile\.in=)
       {
-        $NeedConfig = 1;
+        $NeedMakefile = 1;
       }
-      elsif ($Line =~ m=include/.*\.idl=)
+      elsif ($Line =~ m=^diff.*include/.*\.idl=)
       {
         $NeedMakeInclude = 1;
       }
-      elsif ($Line =~ m=.spec=)
+      elsif ($Line =~ m=^diff.*\.spec=)
       {
         $NeedBuildDeps = 1;
       }
-      elsif ($PatchType eq "dlls" && $Line =~ m=$BaseName/Makefile\.in=)
+      elsif ($PatchType eq "dlls" && $Line =~ m=^diff.*$BaseName/Makefile\.in=)
       {
         $NeedImplib = 1;
+      }
+      elsif ($Line =~ m=^diff.*configure\.ac=)
+      {
+        $NeedAutoconf = 1;
+      }
+      elsif ($Line =~ m=^diff.*configure=)
+      {
+        $NeedConfigure = 1;
       }
     }
     close FH;
   }
 
-  LogMsg "Applying patch\n";
+  InfoMsg "Applying patch\n";
   system("git apply --verbose --directory=$DataDir/wine-git $PatchFile " .
          ">> $LogDir/BuildSingleTest.log 2>&1");
   if ($? != 0)
   {
     LogMsg "Patch failed to apply\n";
-    return (-1, $NeedMakeInclude);
+    return (-1, $NeedMakeInclude, $NeedBuildDeps, $NeedImplib, $NeedConfigure);
   }
 
-  return ($NeedConfig, $NeedMakeInclude, $NeedBuildDeps, $NeedImplib);
+  if ($NeedAutoconf && ! $NeedConfigure)
+  {
+    InfoMsg "Running autoconf\n";
+    system("cd $DataDir/wine-git; autoconf --output configure configure.ac" .
+           ">> $LogDir/BuildSingleTest.log 2>&1");
+    if ($? != 0)
+    {
+       LogMsg "Autoconf failed\n";
+       return (-1, $NeedMakeInclude, $NeedBuildDeps, $NeedImplib,
+               $NeedConfigure);
+    }
+    $NeedConfigure = 1;
+  }
+
+  return ($NeedMakefile, $NeedMakeInclude, $NeedBuildDeps, $NeedImplib,
+          $NeedConfigure);
 }
 
 sub BuildTestExecutable
 {
-  my ($BaseName, $PatchType, $Bits, $NeedConfig, $NeedMakeInclude,
-      $NeedBuildDeps, $NeedImplib) = @_;
+  my ($BaseName, $PatchType, $Bits, $NeedConfigure, $NeedMakefile,
+      $NeedMakeInclude, $NeedBuildDeps, $NeedImplib) = @_;
 
-  if ($NeedMakeInclude)
+  if ($NeedConfigure)
   {
-    LogMsg "Performing reconfig in include dir\n";
+    InfoMsg "Reconfigure $Bits-bit crossbuild\n";
+    my $Host = ($Bits == 64 ? "x86_64-w64-mingw32" : "i686-pc-mingw32");
+    system("cd $DataDir/build-mingw$Bits && " .
+           "../wine-git/configure --host=$Host " .
+           "--with-wine-tools=../build-native --without-x --without-freetype " .
+           ">> $LogDir/BuildSingleTest.log 2>&1");
+    if ($? != 0)
+    {
+      LogMsg "Reconfigure of $Bits-bit crossbuild failed\n";
+      return !1;
+    }
+  }
+
+  if ($NeedMakeInclude || $NeedConfigure)
+  {
+    InfoMsg "Recreating include/Makefile\n";
     system("cd $DataDir/build-mingw${Bits}; ./config.status --file include/Makefile:Make.vars.in:include/Makefile.in >> $LogDir/BuildSingleTest.log 2>&1");
     if ($? != 0)
     {
-      LogMsg "Reconfig in include dir failed\n";
+      LogMsg "Recreation of include/Makefile failed\n";
       return !1;
     }
 
@@ -101,9 +152,9 @@ sub BuildTestExecutable
     }
   }
 
-  if ($NeedImplib)
+  if ($NeedImplib || $NeedConfigure)
   {
-    LogMsg "Rebuilding $BaseName import lib\n";
+    InfoMsg "Rebuilding $BaseName import lib\n";
     system("cd $DataDir/build-mingw${Bits}; ./config.status --file $PatchType/$BaseName/Makefile:Make.vars.in:$PatchType/$BaseName/Makefile.in >> $LogDir/BuildSingleTest.log 2>&1");
     if ($? != 0)
     {
@@ -120,24 +171,24 @@ sub BuildTestExecutable
     }
   }
 
-  if ($NeedConfig)
+  if ($NeedMakefile || $NeedConfigure)
   {
-    LogMsg "Performing tests reconfig\n";
+    InfoMsg "Recreating tests/Makefile\n";
     system("cd $DataDir/build-mingw${Bits}; ./config.status --file $PatchType/$BaseName/tests/Makefile:Make.vars.in:$PatchType/$BaseName/tests/Makefile.in >> $LogDir/BuildSingleTest.log 2>&1");
     if ($? != 0)
     {
-      LogMsg "Reconfig failed\n";
+      LogMsg "Recreation of tests/Makefile failed\n";
       return !1;
     }
   }
 
   if ($NeedBuildDeps)
   {
-    LogMsg "Making build dependencies\n";
+    InfoMsg "Making build dependencies\n";
     system("cd $DataDir/build-mingw${Bits}; make __builddeps__ >> $LogDir/BuildSingleTest.log 2>&1");
     if ($? != 0)
     {
-      LogMsg "Reconfig failed\n";
+      LogMsg "Making build dependencies failed\n";
       return !1;
     }
   }
@@ -151,7 +202,7 @@ sub BuildTestExecutable
   $TestExecutable .= "_test.exe";
   unlink($TestExecutable);
  
-  LogMsg "Making test executable\n";
+  InfoMsg "Making test executable\n";
   system("make -C $TestsDir >> $LogDir/BuildSingleTest.log 2>&1");
   if ($? != 0)
   {
@@ -241,20 +292,22 @@ else
   FatalError "Invalid number of bits $BitIndicators\n";
 }
 
-my ($NeedConfig, $NeedMakeInclude, $NeedBuildDeps, $NeedImplib) = 
-  ApplyPatch($PatchFile, $PatchType, $BaseName);
-if ($NeedConfig < 0)
+my ($NeedMakefile, $NeedMakeInclude, $NeedBuildDeps, $NeedImplib,
+    $NeedConfigure) = ApplyPatch($PatchFile, $PatchType, $BaseName);
+if ($NeedConfigure < 0)
 {
   exit(1);
 }
 
-if ($Run32 && ! BuildTestExecutable($BaseName, $PatchType, 32, 0 < $NeedConfig,
+if ($Run32 && ! BuildTestExecutable($BaseName, $PatchType, 32,
+                                    $NeedConfigure, 0 < $NeedMakefile,
                                     $NeedMakeInclude, $NeedBuildDeps,
                                     $NeedImplib))
 {
   exit(1);
 }
-if ($Run64 && ! BuildTestExecutable($BaseName, $PatchType, 64, 0 < $NeedConfig,
+if ($Run64 && ! BuildTestExecutable($BaseName, $PatchType, 64,
+                                    $NeedConfigure, 0 < $NeedMakefile,
                                     $NeedMakeInclude, $NeedBuildDeps,
                                     $NeedImplib))
 {
