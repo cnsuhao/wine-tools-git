e7ee02677cc9621782f07b26e4df261dd6b30316
Francois Gouget <fgouget@codeweavers.com>
2012-10-05 11:16:26 +0200

    testbot/build: Use the standard MinGW compiler names in the Reconfig.pl script.

--- testbot/bin/Reconfig.pl
+++ testbot/bin/Reconfig.pl
@@ -86,7 +86,7 @@ sub BuildCross
 {
   my $Bits = $_[0];
 
-  my $Host = ($Bits == 64 ? "x86_64-w64-mingw32" : "i686-pc-mingw32");
+  my $Host = ($Bits == 64 ? "x86_64-w64-mingw32" : "i686-w64-mingw32");
   mkdir "$DataDir/build-mingw$Bits" if (! -d "$DataDir/build-mingw$Bits");
   system("cd $DataDir/build-mingw$Bits && " .
          "rm -rf * && " .
