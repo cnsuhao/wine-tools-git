63271ccb5a4852edf566ac9a3f73733ada472b95
Jacek Caban <jacek@codeweavers.com>
2014-09-03 13:36:49 +0200

    gzip.php: Added a file for compressed connection tests.

---
+++ winetest/tests/gzip.php
@@ -0,0 +1,15 @@
+<?php
+header("Cache-Control: no-store");
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Page for winetest tests purposes</title>
+</head>
+<body>
+<?php
+echo str_repeat("test ", 10000);
+?>
+</body>
+</html>
